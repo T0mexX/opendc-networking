83a31b36591e03c435f6600861a73ff6545860a6
t0m3x <tomex5001@gmail.com>
2024-06-27 18:19:51 +0200

    test(simulator-network): add test for CustomNetwork and FatTreeNetwork

---
+++ opendc-simulator/opendc-simulator-network/src/test/kotlin/org/opendc/simulator/network/CustomNetworkTopologyTest.kt
@@ -0,0 +1,108 @@
+package org.opendc.simulator.network
+
+import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.common.ExperimentalKotest
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.withData
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
+import io.kotest.matchers.collections.shouldContainExactlyInAnyOrder
+import io.kotest.property.Arb
+import io.kotest.property.arbitrary.bind
+import io.kotest.property.arbitrary.int
+import io.kotest.property.arbitrary.next
+import io.kotest.property.checkAll
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromStream
+import org.opendc.simulator.network.components.CustomNetwork
+import org.opendc.simulator.network.components.CustomNetwork.CustomNetworkSpecs
+import org.opendc.simulator.network.components.Network
+import org.opendc.simulator.network.components.Node
+import org.opendc.simulator.network.components.NodeId
+import org.opendc.simulator.network.components.Specs
+import org.opendc.simulator.network.components.Switch
+import java.io.File
+
+@OptIn(ExperimentalSerializationApi::class, ExperimentalKotest::class)
+class CustomNetworkTopologyTest: FunSpec({
+    val jsonReader = Json { ignoreUnknownKeys = true }
+
+    context("Build network from json file") {
+
+        File("src/test/resources/custom-network/specs-test1.json").let { testFile ->
+            context("convert json file to network specs").config(enabled = testFile.exists()) {
+                shouldNotThrowAny { jsonReader.decodeFromStream<Specs<Network>>(testFile.inputStream()) }
+            }
+        }
+
+        File("src/test/resources/custom-network/specs-test2.json").let { testFile ->
+            context("build network from specs").config(enabled = testFile.exists()) {
+                shouldNotThrowAny { jsonReader.decodeFromStream<Specs<Network>>(testFile.inputStream()).buildFromSpecs() }
+            }
+        }
+
+        context("build network from incorrect specs") {
+            File("src/test/resources/custom-network/specs-duplicate-node-id.json").let { testFile ->
+                context("duplicate node ids (eliminating duplicates)").config(enabled = testFile.exists()) {
+                    shouldNotThrowAny { jsonReader.decodeFromStream<Specs<Network>>(testFile.inputStream()).buildFromSpecs() }
+                }
+            }
+
+            File("src/test/resources/custom-network/specs-undeployable-link.json").let { testFile ->
+                context("link between non-existing nodes (ignoring)").config(enabled = testFile.exists()) {
+                    shouldNotThrowAny { jsonReader.decodeFromStream<Specs<Network>>(testFile.inputStream()).buildFromSpecs() }
+                }
+            }
+
+            File("src/test/resources/custom-network/specs-bad-link-array-sizes.json").let { testFile ->
+                context("link arrays sizes not equal 2").config(enabled = testFile.exists()) {
+                    shouldNotThrowAny {
+                        val customNet: CustomNetworkSpecs =
+                            jsonReader.decodeFromStream<Specs<CustomNetwork>>(testFile.inputStream()) as CustomNetworkSpecs
+                        customNet.links shouldContainExactlyInAnyOrder listOf(Pair(0, 3), Pair(0, 4))
+                    }
+                }
+            }
+        }
+    }
+
+    context("build correct links from link list") {
+        data class TestData(val nodes: List<Node>, val linkList: List<Pair<NodeId, NodeId>>)
+        fun Node.isConnectedTo(other: Node): Boolean = this.linksToAdjNodes.contains(other.id)
+
+        val testDataGenerator: Arb<TestData> = Arb.bind(
+            Arb.int(2..20),
+            Arb.int(1..10)
+        ) { numOfNodes, numOfLinks ->
+            val nodes: List<Node> = buildList {
+                (0..<numOfNodes).forEach { id ->  add(Switch(id = id, numOfPorts = numOfNodes, portSpeed = .0)) }
+            }
+            val links: List<Pair<NodeId, NodeId>> = buildList {
+                val arbNodeId = Arb.int(0..<numOfNodes)
+                repeat(numOfLinks) { add(Pair(arbNodeId.next(), arbNodeId.next())) }
+            }
+            TestData(nodes, links)
+        }
+
+        checkAll(iterations = 100 ,testDataGenerator) { testData ->
+            context("Test with nodes ${testData.nodes} and links ${testData.linkList}") {
+                val nodes: List<Node> = testData.nodes
+                val links: List<Pair<NodeId, NodeId>> = testData.linkList
+                val network = CustomNetwork(nodes)
+                network.connectFromLinkList(links)
+                withData(links) { link ->
+                    if (link.first != link.second) {
+                        network.nodes[link.first]
+                            ?.isConnectedTo(network.nodes[link.second]!!)
+                            ?.shouldBeTrue()
+                    } else {
+                        network.nodes[link.first]
+                            ?.isConnectedTo(network.nodes[link.second]!!)
+                            ?.shouldBeFalse()
+                    }
+                }
+            }
+        }
+    }
+})
---
+++ opendc-simulator/opendc-simulator-network/src/test/kotlin/org/opendc/simulator/network/FatTreeNetworkTopologyTest.kt
@@ -0,0 +1,54 @@
+package org.opendc.simulator.network
+
+import io.kotest.assertions.throwables.shouldNotThrowAny
+import io.kotest.common.ExperimentalKotest
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.datatest.withData
+import io.kotest.matchers.ints.shouldBeExactly
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.decodeFromStream
+import org.opendc.simulator.network.components.FatTreeNetwork
+import org.opendc.simulator.network.components.FatTreeNetwork.FatTreeTopologySpecs
+import org.opendc.simulator.network.components.Network
+import org.opendc.simulator.network.components.Specs
+import org.opendc.simulator.network.components.Switch.SwitchSpecs
+import java.io.File
+import kotlin.math.pow
+
+@OptIn(ExperimentalSerializationApi::class, ExperimentalKotest::class)
+class FatTreeNetworkTopologyTest: FunSpec({
+    val jsonReader = Json { ignoreUnknownKeys = true }
+
+    context("convert json file to network specs") {
+        File("src/test/resources/fat-tree-network/specs-k4.json").let { testFile ->
+            context("convert json with generic switch specs").config(enabled = testFile.exists()) {
+                shouldNotThrowAny { jsonReader.decodeFromStream<Specs<Network>>(testFile.inputStream()) }
+            }
+        }
+
+        File("src/test/resources/fat-tree-network/specs-k4-override-layer.json").let { testFile ->
+            context("convert json with per layer switch specs").config(enabled = testFile.exists()) {
+                shouldNotThrowAny { jsonReader.decodeFromStream<Specs<Network>>(testFile.inputStream()).buildFromSpecs() }
+            }
+        }
+    }
+
+    context("build fat-tree from specs") {
+        withData(
+            SwitchSpecs(numOfPorts = 4, portSpeed = .0),
+            SwitchSpecs(numOfPorts = 6, portSpeed = .0),
+            SwitchSpecs(numOfPorts = 8, portSpeed = .0),
+            SwitchSpecs(numOfPorts = 10, portSpeed = .0)
+        ) { switchSpecs ->
+            val k: Int = switchSpecs.numOfPorts
+            val fatTree: FatTreeNetwork = FatTreeTopologySpecs(switchSpecs = switchSpecs).buildFromSpecs()
+            fatTree.pods.size shouldBeExactly k
+            fatTree.leafs.size shouldBeExactly (k / 2).toDouble().pow(2).toInt() * k
+            fatTree.torSwitches.size shouldBeExactly k * k / 2
+            fatTree.aggregationSwitches.size shouldBeExactly k * k / 2
+            fatTree.coreSwitches.size shouldBeExactly k * k / 4
+            fatTree.networkEndPointNodes.size shouldBeExactly (k * k * k) / 4 + k * k / 4
+        }
+    }
+})
