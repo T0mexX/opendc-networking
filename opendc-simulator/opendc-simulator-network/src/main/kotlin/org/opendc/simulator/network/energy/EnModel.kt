539cb84ae7d704fe6901142c5e8e215d1386cacf
t0m3x <tomex5001@gmail.com>
2024-06-27 14:43:09 +0200

    feat(simulator-network): add energy related initial interfaces and classes

---
+++ opendc-simulator/opendc-simulator-network/src/main/kotlin/org/opendc/simulator/network/energy/EnModel.kt
@@ -0,0 +1,15 @@
+package org.opendc.simulator.network.energy
+
+import org.opendc.simulator.network.utils.Watts
+
+/**
+ * Represents the energy model for a specific component of type `T`.
+ */
+internal fun interface EnModel<T: EnergyConsumer<T>> {
+
+    /**
+     * Computes the current energy consumption of ***this***.
+     * @param[enConsumer]   energy consumer network component whose energy consumption is to be computed.
+     */
+    fun computeCurrConsumpt(enConsumer: T): Watts
+}
---
+++ opendc-simulator/opendc-simulator-network/src/main/kotlin/org/opendc/simulator/network/energy/EnMonitor.kt
@@ -0,0 +1,41 @@
+package org.opendc.simulator.network.energy
+
+import org.opendc.simulator.network.utils.OnChangeHandler
+import org.opendc.simulator.network.utils.Watts
+import kotlin.properties.Delegates
+
+/**
+ * Monitors the energy consumption of [monitored] of type `T`.
+ * @param[monitored]    network component monitored by ***this***.
+ * @param[enModel]      energy model to use to compute current energy consumption for [monitored] network component.
+ * TODO: maybe store records of energy changes, this can be done on network level or both component level and network.
+ */
+internal class EnMonitor<T: EnergyConsumer<T>>(
+    private val monitored: T,
+    private val enModel: EnModel<T> = monitored.getDfltEnModel()
+) {
+    /**
+     * Callback functions of the observers of the [currConsumpt] field.
+     */
+    private val obs: MutableList<OnChangeHandler<EnMonitor<T>, Watts>> = mutableListOf()
+
+    /**
+     * The current energy consumption of the [monitored] network component.
+     */
+    private var currConsumpt: Watts by Delegates.observable(.0) { _, oldValue, newValue ->
+        obs.forEach { it.handleChange(this, oldValue, newValue) }
+    }
+
+    /**
+     * Adds an observer callback function.
+     * @param[f]    callback function of the new observer.
+     */
+    fun addObserver(f: OnChangeHandler<EnMonitor<T>, Watts>) { obs.add(f) }
+
+    /**
+     * Updates the energy consumption of [monitored].
+     *
+     * Ideally called by [monitored] whenever its state changes.
+     */
+    fun update() { currConsumpt = enModel.computeCurrConsumpt(monitored) }
+}
