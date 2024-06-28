5f33d3be86371d5d9a2faa41123d43cf0b1da709
t0m3x <tomex5001@gmail.com>
2024-06-28 16:03:10 +0200

    feat(simulator-network): now logger can be instantiated with custom name

--- opendc-simulator/opendc-simulator-network/src/main/kotlin/org/opendc/simulator/network/utils/Logger.kt
+++ opendc-simulator/opendc-simulator-network/src/main/kotlin/org/opendc/simulator/network/utils/Logger.kt
@@ -6,6 +6,6 @@ import org.slf4j.Logger
 /**
  * Returns a [Logger] with the class name of the caller, even if the caller is a companion object.
  */
-internal fun <T: Any> T.logger(): Lazy<Logger> {
-    return lazy { KotlinLogging.logger(this::class.java.enclosingClass.simpleName)}
+internal fun <T: Any> T.logger(name: String? = null): Lazy<Logger> {
+    return lazy { KotlinLogging.logger(name ?: this::class.java.enclosingClass.simpleName)}
 }
