1e8a95d8143fe55ec6a69c59b9cd917c66c0365f
t0m3x <tomex5001@gmail.com>
2024-07-22 11:12:06 +0200

    docs(sim-net): add documnetation to `StabilityValidator`

--- opendc-simulator/opendc-simulator-network/src/main/kotlin/org/opendc/simulator/network/components/StabilityValidator.kt
+++ opendc-simulator/opendc-simulator-network/src/main/kotlin/org/opendc/simulator/network/components/StabilityValidator.kt
@@ -3,29 +3,59 @@ package org.opendc.simulator.network.components
 import kotlinx.coroutines.sync.Mutex
 import kotlinx.coroutines.sync.withLock
 
+/**
+ * Tracks the network stability. Each instance of [Invalidator]
+ * can invalidate its own state and consequently the
+ * state of the network (if undergoing some update, connection, etc.).
+ *
+ * One can wait for the network to be stable with [awaitStability].
+ */
 internal class StabilityValidator {
 
+    /**
+     * Locked when the network is not stable.
+     */
     private var stabilityLock = Mutex()
 
+    /**
+     * Number of [Invalidator]s that have currently invalidated the network.
+     * When this field is 0 the network is stable and [stabilityLock] is unlocked.
+     */
     private var invalidCount: Int = 0
     private var countLock = Mutex()
 
+    /**
+     * Suspend method that suspends until the network reached a stable state.
+     */
     internal suspend fun awaitStability() {
         stabilityLock.lock()
         stabilityLock.unlock()
     }
 
+    /**
+     * Resets [invalidCount]. Old [Invalidator]s can still invalidate the state of the network.
+     */
     internal fun reset() {
         invalidCount = 0
         countLock = Mutex()
         stabilityLock = Mutex()
     }
 
-
-    // assumed to be used synchronously
+    /**
+     * Instances of this class can invalidate their state,
+     * and consequently, the state of the network.
+     */
     internal inner class Invalidator {
+        /**
+         * Keeps track of the last state of this invalidator.
+         * [invalidate] and [validate] are assumed to be used synchronously.
+         */
         private var isValid = true
 
+        /**
+         * Invalidates the state of this instance if not already invalidated.
+         * The network state will not be valid until all invalidators validate their own state.
+         */
         suspend fun invalidate() {
             if (isValid.not()) return
             countLock.withLock {
@@ -36,6 +66,10 @@ internal class StabilityValidator {
             isValid = isValid.not()
         }
 
+        /**
+         * Validates the state of this instance if not already validated.
+         * The network will not be valid until all validators validate their own state.
+         */
         suspend fun validate() {
             if (isValid) return
             countLock.withLock {
