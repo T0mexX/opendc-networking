4713d1a462b13ee244935379bbf4119c78b4be61
t0m3x <tomex5001@gmail.com>
2024-08-28 11:39:26 +0200

    Removed trace-api-new from network-master

--- opendc-trace/opendc-trace-api-new/src/main/kotlin/org/opendc/trace/table/column/Column.kt
+++
@@ -1,8 +0,0 @@
-package org.opendc.trace.table.column
-
-import org.opendc.trace.table.column.ColumnReader.ColumnType
-
-public abstract class Column<O> {
-    public abstract val name: String
-    public abstract val type: ColumnType<O>
-}
